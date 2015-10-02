632462bf0fd09a70df95e8f68bf3898acad4c2e6
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2015-10-02 10:21:18 +0200

    Email validator bug fixed

--- library/src/main/java/com/marcohc/helperoid/StringHelper.java
+++ library/src/main/java/com/marcohc/helperoid/StringHelper.java
@@ -15,7 +15,7 @@ public class StringHelper {
      * @param hex hex for validation
      * @return true valid hex, false invalid hex
      */
-    public static boolean validateEmail(final String hex) {
+    public static boolean isValidEmail(final String hex) {
         if (matcher == null || pattern == null) {
             pattern = Pattern.compile(EMAIL_PATTERN);
             matcher = pattern.matcher(hex);
