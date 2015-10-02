17b59abe5880d9232349f3668324318f5c4be9c8
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2015-10-02 10:32:17 +0200

    Email validator bug fixed 2

--- library/src/main/java/com/marcohc/helperoid/StringHelper.java
+++ library/src/main/java/com/marcohc/helperoid/StringHelper.java
@@ -1,26 +1,23 @@
 package com.marcohc.helperoid;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringHelper {
 
     private static Pattern pattern;
-    private static Matcher matcher;
     private static final String EMAIL_PATTERN = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
     /**
      * Validate hex with regular expression
      *
-     * @param hex hex for validation
+     * @param email hex for validation
      * @return true valid hex, false invalid hex
      */
-    public static boolean isValidEmail(final String hex) {
-        if (matcher == null || pattern == null) {
+    public static boolean isEmailValid(final String email) {
+        if (pattern == null) {
             pattern = Pattern.compile(EMAIL_PATTERN);
-            matcher = pattern.matcher(hex);
         }
-        return matcher.matches();
+        return pattern.matcher(email).matches();
     }
 
     public static boolean isEmpty(final String text) {
